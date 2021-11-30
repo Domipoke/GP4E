9c4bf4c2f9b3b4c725e9f0d618bdffdfaf516678
Domipoke <domenico.montuori1312006@gmail.com>
2021-11-30 18:15:39 +0100

    update

--- utility/switch.js
+++ utility/switch.js
@@ -6,10 +6,25 @@
  * or from
  *  b to a
  */
-function switch_(w,a,b) {
+function s(w,a,b) {
     if (w==a) {
         return b
     } else {
         return a
+    }
+}
+/**
+ * Switch 
+ *  this
+ * from 
+ *  a to b
+ * or from
+ *  b to a
+ */
+function cs(w,a,b,ae,be) {
+    if (w==a) {
+        return be
+    } else {
+        return ae
     }
 }
